105c5a3b465b8c16862776b474ac4e72fb57b6ed
francisco <franciscoolivaresrivera@gmail.com>
2018-11-29 00:26:31 -0300

    Notas Faltantes

--- valve/prop_valve.py
+++ valve/prop_valve.py
@@ -1,51 +1,48 @@
 #!/usr/bin/env python
 # servo.py
-import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
-import time                #Importamos time para poder usar time.sleep
+import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
-GPIO.setup(11,GPIO.OUT)    #Ponemos el pin 21 como salida
-p = GPIO.PWM(11,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
-p.start(3)               #Enviamos un pulso del 7.5% para centrar el servo
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(11,GPIO.OUT)
+p = GPIO.PWM(11,50)
+p.start(3)
 state = 3
-
-try :
-    temperature = raw_input('Enter temperature :')
-    temperature = float(temperature)
-    limsup = temperature + 3
-    liminf = temperature - 3
-    p.ChangeDutyCycle(3)
-    while True:
-        try:
-                tempfile = open("/sys/bus/w1/devices/28-020e9177de9d/w1_slave")
-                thetext = tempfile.read()
-                tempfile.close()
-                tempdata = thetext.split("\n")[1].split(" ")[9]
-                temp = float(tempdata[2:])
-                temp = temp / 1000
-                print('Temperature : %s'%temp)
-                if temp <= liminf and state <9:
-                        state = state +1
-                        p.ChangeDutyCycle(state)
-                        time.sleep(1)
-                elif temp >= limsup and state > 3:
-                        state = state -1
-                        p.ChangeDutyCycle(state)
-                        time.sleep(1)
-                else:
-                        time.sleep(1)
-        except KeyboardInterrupt:
-               p.stop()
-               GPIO.cleanup()
-
-        p.ChangeDutyCycle(state)
-        time.sleep(0.5)
-except ValueError:
-        temperature = raw_input('Enter a valid temperature :')
-	temperature = float(temperature)
+while true:
+    try :
+        temperature = raw_input('Enter temperature :') #debiera obtenerla del websocket o servidor
+        temperature = float(temperature)
         limsup = temperature + 3
         liminf = temperature - 3
+        p.ChangeDutyCycle(3)
+        while True:
+            try:
+                    tempfile = open("/sys/bus/w1/devices/28-020e9177de9d/w1_slave") # Agregar segundo sensor que mida la temperatura del termo
+                    thetext = tempfile.read()
+                    tempfile.close()
+                    tempdata = thetext.split("\n")[1].split(" ")[9]
+                    temp = float(tempdata[2:])
+                    temp = temp / 1000
+                    print('Temperature : %s'%temp) # enviar temperatura del agua al servidor, seria la temperatura que esta calentando
+                    if temp <= liminf and state <9:
+                            state = state +1
+                            p.ChangeDutyCycle(state)
+                            time.sleep(1)   # tiempo que se duerme hasta que toma la siguiente temperatura y mueve la valvula debiera ser 3 seg
+                    elif temp >= limsup and state > 3.5:
+                            state = state -1
+                            p.ChangeDutyCycle(state)
+                            time.sleep(1)
+                    else:
+                            time.sleep(1)
+            except KeyboardInterrupt:
+                   p.stop()
+                   GPIO.cleanup()
+
+            p.ChangeDutyCycle(state)
+            time.sleep(0.5)
+    except ValueError:
+            continue
+    break
 
 p.stop()
 GPIO.cleanup()
